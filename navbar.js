e802b939268381114d64bfbfa76ac6867182978e
Staci Shon <staci.shon@gmail.com>
2020-11-03 11:22:50 -0600

    coded out creating table for schema

---
+++ navbar.js
@@ -0,0 +1,18 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+const express = require("express");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
