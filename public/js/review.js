220e35f65dc99c15bf730fc749bb94f3762fc788
Staci Shon <staci.shon@gmail.com>
2020-11-05 10:10:46 -0600

    added fontawesome CDN, added edit+delete button

--- public/js/review.js
+++ public/js/review.js
@@ -13,8 +13,7 @@ $(document).ready(function() {
         $("#author").append("<option value='1'>"+data.username+"</option>");
     });
 
-    const submit = document.getElementById('review');
-    $(submit).on("submit", handleFormSubmit)
+    $("#review").on("submit", handleFormSubmit);
 
     // A function for handling what happens when the form to create a new post is submitted
     function handleFormSubmit(event) {
@@ -33,14 +32,6 @@ $(document).ready(function() {
             return;
         }
 
-
-        // If we're updating a post run updatePost to update a post
-        // Otherwise run submitPost to create a whole new post
-        // if (updating) {
-        //     newPost.id = postId;
-        //     updatePost(newPost);
-        // }
-        // else {
         submitPost(newPost.song, newPost.artist, newPost.album, newPost.body, newPost.rating, newPost.author);
         titleInput.val("");
         artistInput.val("");
@@ -48,26 +39,7 @@ $(document).ready(function() {
         bodyInput.val("");
         ratingSelect.val("1");
         authorSelect.val("0");
-        // }
     }
-
-//     function submitPost(post) {
-//         console.log('post', post)
-//         $.post("/api/review", post, function(response) {
-//             console.log(response)
-//             // window.location.href = "/review";
-//         });
-//     }
-
-//     submitPost(newPost);
-//     titleInput.val("");
-//     artistInput.val("");
-//     albumInput.val("");
-//     bodyInput.val("");
-//     ratingSelect.val("1");
-//     authorSelect.val("0");
-//     // }
-// }
 
 function submitPost(song, artist, album, body, rating, author) {
     console.log('hello2');
