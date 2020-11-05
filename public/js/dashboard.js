3e63399059b4c42f9a6d4313486bc28ab8129b57
Julian Rivera Jr <julianriverajr@gmail.com>
2020-11-04 21:17:42 -0600

    tried to adjust dash js code

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -19,11 +19,11 @@ Rating: ${data.rating} <br>
 });
 /*
 for each review by the logged in user,
-create a div like the one starting on line 10 displaying the review 
-and other such info;
+append a div like the one starting on line 10 displaying the review 
+and other such info to the #actualReview article;
 i have to grab from the review db
 will the users have their own db with only their reviews? ASK THIS, JULIAN
-if so then the code should work i hope and think;
-oh, how do i write the for function? ASK THIS, JULIAN
-I need to use the reviewRoutes const in my actual code, how? ASK THIS JULIAN
+if so then the code might work i hope and think;
+oh, how do i write the for function? like, what do i put after '<'? ASK THIS, JULIAN
+I need to use the reviewRoutes const in my actual code, how/where? ASK THIS JULIAN
 */
