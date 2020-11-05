e1bb6df55c49713d7ab85957156242ce512cfee7
Julian Rivera Jr <julianriverajr@gmail.com>
2020-11-04 21:21:16 -0600

    removed pseudo code

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -17,13 +17,3 @@ Rating: ${data.rating} <br>
         }
     })
 });
-/*
-for each review by the logged in user,
-append a div like the one starting on line 10 displaying the review 
-and other such info to the #actualReview article;
-i have to grab from the review db
-will the users have their own db with only their reviews? ASK THIS, JULIAN
-if so then the code might work i hope and think;
-oh, how do i write the for function? like, what do i put after '<'? ASK THIS, JULIAN
-I need to use the reviewRoutes const in my actual code, how/where? ASK THIS JULIAN
-*/
